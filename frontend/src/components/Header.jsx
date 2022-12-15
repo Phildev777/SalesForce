456bcdd7b1e80e0c8b91931c4fc8c359b849feb6
ncesari49 <cesarin@hotmail.fr>
2022-12-15 15:06:47 +0100

    add some style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,38 @@
 import React from "react";
+import "../assets/styles/header.css";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [nav, setNav] = React.useState(false);
+  const handleNav = () => setNav(!nav);
   return (
-    <header>
-      <div className="containerHeaderImg">
-        <img alt="profil" />
-
-        <h1>test</h1>
-      </div>
-      <ul>
-        <li>
-          <Link to="/mon espace">Mon espace</Link>
-        </li>
-        <li>
-          <Link to="/idees">Idées</Link>
-        </li>
-        <li>
-          <Link to="/utilisateurs">Utilisateurs</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/admin">Admin</Link>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className={`${nav ? "show" : "hide"}`}>
+        <div className="containerHeaderImg">
+          <img alt="profil" />
+        </div>
+        <ul>
+          <li>
+            <Link to="/mon espace">Mon espace</Link>
+          </li>
+          <li>
+            <Link to="/idees">Idées</Link>
+          </li>
+          <li>
+            <Link to="/utilisateurs">Utilisateurs</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin</Link>
+          </li>
+        </ul>
+      </header>
+      <button className="burger" type="button" onClick={handleNav}>
+        <span className="bugerIcon" />
+      </button>
+    </>
   );
 }
 

d849beef99aa5c924baae06069e42d0dca1e022c
ncesari49 <cesarin@hotmail.fr>
2022-12-16 10:24:07 +0100

    create Utilisateurs and his childreen

--- frontend/src/pages/Utilisateurs.jsx
+++ frontend/src/pages/Utilisateurs.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import UtilisateurCarte from "@components/UtilisateurCarte";
+import "../assets/styles/Utilisateurs.css";
 
 function Utilisateurs() {
-  return <div>utiilisateur</div>;
+  return (
+    <div className="utilisateursContainer">
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+      <UtilisateurCarte />
+    </div>
+  );
 }
 
 export default Utilisateurs;

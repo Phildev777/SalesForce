e2d3bf87d8edb6670c907c7172ab72cde9b07b80
Adam Giromanay <adam202009@live.fr>
2022-12-16 12:07:36 +0100

    no modif photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "./pages/Home";
-
 import "./App.css";
+import MyProfile from "./components/MyProfile";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>hello</p>
+      <MyProfile />
     </div>
   );
 }

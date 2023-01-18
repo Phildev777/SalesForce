b408f1d53dfecfc60412c118ceed4bafdcdd7e7d
Adam Giromanay <adam202009@live.fr>
2023-01-18 11:13:52 +0100

    backend with getprofile

--- backend/src/routes/utilisateurs.route.js
+++ backend/src/routes/utilisateurs.route.js
@@ -1,48 +1,15 @@
 const router = require("express").Router();
 
 const utilisateurController = require("../controllers/utilisateurs.controller");
+const profilController = require("../controllers/profilController");
 
 router.get("/", utilisateurController.getAllUtilisateurs);
+router.get("/profile", profilController.getAllProfiles);
 router.get("/:id", utilisateurController.getUtilisateurById);
+
 router.post("/login", utilisateurController.login);
 router.post("/creation", utilisateurController.createUtilisateur);
 router.put("/", utilisateurController.updateUtilisateur);
 router.delete("/:id", utilisateurController.deleteUtilisateur);
 
 module.exports = router;
-
-/*  {
-            
-            "nom": "eliot",
-            "prenom": "Kel",
-            "dateembauche": "2020-04-29",
-            "motdepasse": "machin",
-            "admin": 0,
-            "anniversaire": "2000-09-01",
-            "serviceIdservice": 9,
-            "fonctionIdfonction": 2,
-            "email": "mach@huffingtonpost.com"
-        } */
-/*       {
-          "nom": "Stark",
-          "prenom": "Tony",
-          "dateembauche": "2020-04-29",
-          "motdepasse": "avengers",
-          "admin": 1,
-          "anniversaire": "2000-09-01",
-          "serviceIdservice": 9,
-          "fonctionIdfonction": 2,
-          "email": "popperpots@huffingtonpost.com"
-          } */
-
-/*       {
-"nom": "Rogers",
-"prenom": "Steeve",
-"dateembauche": "2020-04-29",
-"motdepasse": "firstav",
-"admin": 1,
-"anniversaire": "2000-09-01",
-"serviceIdservice": 9,
-"fonctionIdfonction": 2,
-"email": "kitten@huffingtonpost.com"
-} */

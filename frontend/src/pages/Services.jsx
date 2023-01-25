5dbfa0ad36a6a920362a2d3011eb8cbd39c24493
Philippe Berra <berraphilippe07@gmail.com>
2023-01-25 18:49:58 +0100

    update context

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import MainContainerTest from "../components/MainContainerTest";
+// import {UserContext} from "../App";
+import UserContext from "../contexts/UserContext";
 
 function Services() {
+  const user = useContext(UserContext);
+
   return (
     <div>
+      <h1>{`Hello${user.token.id} again!`}</h1>
       <MainContainerTest />
     </div>
   );
